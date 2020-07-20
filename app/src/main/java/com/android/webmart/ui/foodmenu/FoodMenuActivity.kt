2d308373705eb4b6d24c55c7a9731ed344220ab1
Haseeb Abdul <abdul.haseeb@cgtindia.in>
2020-07-20 22:05:01 +0530

    Added food menu list

--- app/src/main/java/com/android/webmart/ui/foodmenu/FoodMenuActivity.kt
+++ app/src/main/java/com/android/webmart/ui/foodmenu/FoodMenuActivity.kt
@@ -2,11 +2,42 @@ package com.android.webmart.ui.foodmenu
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.android.webmart.R
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.android.webmart.data.model.Food
+import com.android.webmart.databinding.ActivityFoodMenuBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class FoodMenuActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var viewModel: FoodMenuViewModel
+
+    private lateinit var binding: ActivityFoodMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_food_menu)
+        binding = ActivityFoodMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = this
+    }
+}
+
+@BindingAdapter(value = ["foodItems", "viewModel"], requireAll = true)
+fun foodItems(
+    recyclerView: RecyclerView,
+    list: List<Food>,
+    viewModel: FoodMenuViewModel
+) {
+    if (recyclerView.adapter == null) {
+        recyclerView.adapter = FoodMenuAdapter(viewModel)
+    }
+
+    (recyclerView.adapter as FoodMenuAdapter).apply {
+        submitList(list)
     }
 }
--- app/src/main/java/com/android/webmart/ui/foodmenu/FoodMenuViewModel.kt
+++ app/src/main/java/com/android/webmart/ui/foodmenu/FoodMenuViewModel.kt
@@ -1,8 +1,27 @@
 package com.android.webmart.ui.foodmenu
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.webmart.data.Result
+import com.android.webmart.data.model.Food
+import com.android.webmart.data.source.AppRepository
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class FoodMenuViewModel : ViewModel() {
+class FoodMenuViewModel @Inject constructor(
+    private val repository: AppRepository
+) : ViewModel() {
+
+    private val _foodList = MutableLiveData<List<Food>>().apply {
+        viewModelScope.launch {
+            when(val result = repository.getFoodListFromHotel("hotel1")) {
+                is Result.Success -> value = result.data
+            }
+        }
+    }
+    val foodList: LiveData<List<Food>> = _foodList
 
     /**
      * Called by Data Binding
